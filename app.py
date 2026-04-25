f147e3ff4236674c27556bb23d0f2b0fdc41b844
Andre <andre@email.com>
2026-04-25 11:31:33 +0200

    render fix

--- app.py
+++ app.py
@@ -381,4 +381,6 @@ if __name__ == "__main__":
         init_db()
 
     # Avvio server locale
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
